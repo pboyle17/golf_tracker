aa51562863018fe284486099c54d36776ea62a7c
Paul Boyle <pboyle17@gmail.com>
2016-01-14 18:37:57 -0600

    reading .json file and rendering it on the api route

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var obj={};
+
+fs.readFile('strokes_gained.json',function(err,data){
+  if(err){
+    console.log(err);
+  } else {
+    obj=JSON.parse(data);
+    console.dir(obj);
+  }
+});
 
 router.get('/',function(req,res){
-  res.send('this is the api page');
+  res.send(obj);
 });
 
 module.exports = router;
