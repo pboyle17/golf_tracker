630d8020528f2b083f9ee18938f81aa58276fb47
Paul Boyle <pboyle17@gmail.com>
2016-01-14 20:52:56 -0600

    added putting averages .json file

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var obj={};
+var strokes={};
+var putts={};
+
 
 fs.readFile('strokes_gained.json',function(err,data){
   if(err){
     console.log(err);
   } else {
-    obj=JSON.parse(data);
-    console.dir(obj);
+    strokes=JSON.parse(data);
+    console.dir(strokes);
+  }
+});
+
+fs.readFile('strokes_gained_putting.json',function(err,data){
+  if(err){
+    console.log(err);
+  } else {
+    putts=JSON.parse(data);
+    console.dir(putts);
   }
 });
 
 router.get('/',function(req,res){
-  res.send(obj);
+  res.render('api');
+});
+
+router.get('/strokes',function(req,res){
+  res.send(strokes);
+});
+
+router.get('/putts',function(req,res){
+  res.send(putts);
 });
 
 module.exports = router;
